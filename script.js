e07df7249f380946fe3dd2bb6bc99f271c431983
Udaya Phani Krishna Kondapalli <udayphanikrishna.k@gmail.com>
2020-07-08 16:48:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function resizeCanvas() {
     var ratio =  Math.max(window.devicePixelRatio || 1, 1);
     canvas.width = canvas.offsetWidth * ratio;
     canvas.height = canvas.offsetHeight * ratio;
-    canvas.getContext("2d").scale(ratio, ratio);
+    //canvas.getContext("2d").scale(ratio, ratio);
 }
 
 window.onresize = resizeCanvas;
